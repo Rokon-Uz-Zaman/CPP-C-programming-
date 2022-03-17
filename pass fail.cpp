ca43902fd743c3070c7266d82cae88496da0e62f
A.B.M. Rokon-Uz-Zaman Roman <mdroman266@gmail.com>
2022-03-17 18:16:32 +0600

    Add files via upload

---
+++ pass fail.cpp
@@ -0,0 +1,25 @@
+// pass fail
+#include<conio.h>
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+main()
+{
+int mark ;
+
+string strv;
+//mark=70 ;
+cin >> mark ;
+
+if (mark >= 33)
+ { strv="pass" ;
+ cout<<strv ;
+
+ }
+else
+ {cout<< "fail" ;}
+
+getch();
+}
